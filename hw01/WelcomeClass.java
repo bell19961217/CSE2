a5c4938ceee708a45834696b83956b4d76710c9f
bell19961217 <yid219@lehigh.edu>
2015-09-01 22:44:30 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//Yinchen Dong
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[ ] args) {
+      
+//  display a welcome for me
+    System.out.println("  -----------       ");
+    System.out.println("  | WELCOME |       ");
+    System.out.println("  -----------       ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-Y--I--D--2--1--9-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("I attended my first choral union rehearsal the past Monday. That was awesome!");
+    System.out.println("test");
+    
+    //test/testtest
+  }
+}
